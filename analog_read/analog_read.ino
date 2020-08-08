3e98c77e74f1e5ead3c8a86cdb2a490c1e54633d
Aaron Wood <vega3gx@gmail.com>
2020-08-08 13:22:04 -0700

    photoreceptor changes over time

--- analog_read/analog_read.ino
+++ analog_read/analog_read.ino
@@ -1,12 +1,28 @@
 int analogPin = A3; // potentiometer wiper (middle terminal) connected to analog pin 3
                     // outside leads to ground and +5V
 int val = 0;  // variable to store the value read
+  int led = 9;           // the PWM pin the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
 
 void setup() {
   Serial.begin(9600);           //  setup serial
+  // declare pin 9 to be an output:
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
+  // set the brightness of pin 9:
+  analogWrite(led, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
   val = analogRead(analogPin);  // read the input pin
   Serial.println(val);          // debug value
+  delay(30);
 }
